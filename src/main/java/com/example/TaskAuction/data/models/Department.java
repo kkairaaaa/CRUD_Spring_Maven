bf9d779c4451eb3b3ad7df78a6b6473a77d733e9
kaira <kairatkabdushev@gmail.com>
2023-02-02 10:55:17 +0600

    first commit

---
+++ src/main/java/com/example/TaskAuction/data/models/Department.java
@@ -0,0 +1,7 @@
+package com.example.TaskAuction.data.models;
+
+public enum Department {
+    HUMAN_RESOURCE,
+    MARKETING,
+    ENGINEERING
+}
---
+++ src/main/java/com/example/TaskAuction/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.example.TaskAuction.service;
+
+
+import com.example.TaskAuction.data.models.Employee;
+import com.example.TaskAuction.data.payloads.request.EmployeeRequest;
+import com.example.TaskAuction.data.payloads.response.MessageResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public interface EmployeeService {
+    MessageResponse createEmployee(EmployeeRequest employeeRequest);
+    Optional<Employee> updateEmployee(Integer employeeId, EmployeeRequest employeeRequest);
+    void deleteEmployee(Integer employeeId);
+    Employee getASingleEmployee(Integer employeeId);
+    List<Employee> getAllEmployee();
+}
