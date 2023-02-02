03a02b61be7f250375aa1b69477d83b2996b2938
kaira <kairatkabdushev@gmail.com>
2023-02-02 10:59:15 +0600

    first commit

---
+++ src/test/java/com/example/TaskAuction/TaskAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
